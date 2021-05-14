c50347cb0e40f190be09f76a4264186cc441d123
timardex <timardex@gmail.com>
2021-05-14 12:40:14 +0200

    reload page when View Cart pressed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,20 @@ import './style.scss';
 const Footer = (props) => {  
   const { addToCart, toggleText } = props;
   const disabled = toggleText.includes('Loading');
+  const viewCart = toggleText === 'View Cart';
+
+  const handleClick = () => {
+    if(viewCart) {
+      window.location.reload();
+    }
+    if(!disabled) {
+      addToCart();
+    }
+    return null;
+  }
   return (
     <footer className="footer pt-2 pl-3 pb-3 pr-3">
-      <button onClick={(e) => !disabled ? addToCart() : null} disabled={disabled}>
+      <button onClick={(e) => handleClick()} disabled={disabled}>
         { toggleText }
       </button>
     </footer>
